81f81094815e28314ad3557dcf646ab41c80c6c2
CrCardd <cris.rc377@gmail.com>
2025-05-07 12:02:09 -0300

    desafio 1 completo

---
+++ BIGDATA/Aulas/Aula 1/Levenshtein search Wikipedia/main_simple.py
@@ -0,0 +1,73 @@
+from multiprocessing import Pool
+import math as mt
+import re
+import os
+
+def levenshteinDistance(str1, str2):
+    s1 = len(str1) + 1
+    s2 = len(str2) + 1
+    tab = [[0 for _ in range (s2)] for _ in range(s1)]
+    for x in range(s1):
+        tab[x][0] = x
+    for y in range(s2):
+        tab[0][y] = y
+
+    for x in range(1,s1):
+        for y in range(1, s2):
+            p1 = tab[x][y-1] + 1
+            p2 = tab[x-1][y] + 1
+            p3 = tab[x-1][y-1] + (0 if str1[x-1].lower() == str2[y-1].lower() else 1)
+            tab[x][y] = min(p1,p2,p3)
+
+    return tab[s1-1][s2-1]
+
+
+def process(data):
+    scores = []
+    for archive in data:
+        with open(f'{folder_data}/{archive}', 'r', encoding='utf-8') as arq:
+            content = arq.read()
+        content = re.sub(r'<style.*?>.*?</style>', '', content, flags=re.DOTALL)
+        content = re.sub(r'<script.*?>.*?</script>', '', content, flags=re.DOTALL)
+        content = re.sub(r'<.*?>', '', content, flags=re.DOTALL)
+        lines = content.splitlines()
+
+        with open(f'{folder_cleaned_data}/{archive}', 'w', encoding='utf-8') as cleaned:
+            score = 0
+            scored = False
+            for line in lines:
+                if line.strip() == '':
+                    continue
+                cleaned.write(line.strip() + '\n')
+
+                line = line.split(' ')
+                for word in line:
+                    word = word.strip()
+                    points = levenshteinDistance(search, word)
+                    if points <= metric:
+                        scored = True
+                        points = len(search) - points / len(search)
+                        score += points
+            if scored:
+                score = round(score/len(lines) * 100, 2) 
+                scores.append((archive, score))
+
+        print(archive)
+
+    sorted_values = sorted(scores, key=lambda item: item[1], reverse=True)
+    best_twenty = sorted_values[:20]
+    return best_twenty
+
+folder_data = "./data"
+list_data = os.listdir(folder_data)
+folder_cleaned_data = "./cleaned_data"
+search = 'ascii'
+metric = len(search)/2
+
+results = process(list_data)
+sorted_values = sorted(results, key=lambda item: item[1], reverse=True)
+with open('result_simple.txt', 'w', encoding='utf-8') as result:
+    for score in sorted_values:
+        result.write(f'{score[0].replace(' - Wikipedia.html', '')} \t : {score[1]}\n')
+
+
