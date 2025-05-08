8f356877a08bff21eeb2acde0a09ce19c85b2f3d
CrCardd <cris.rc377@gmail.com>
2025-05-08 10:54:43 -0300

    aula 1 - desafio e resolução definidos

---
+++ BIGDATA/Aulas/Aula 1/Levenshtein search Wikipedia - Challenge/main_simple.py
@@ -0,0 +1,35 @@
+from multiprocessing import Pool
+import math as mt
+import re
+import os
+
+SEARCH = 'data'
+
+folder_data = "./data"
+list_data = os.listdir(folder_data)
+
+def levenshteinDistance(str1, str2):
+    
+    #    +======================+
+    #   || Implemente a função ||
+    #   +======================+
+
+    return None
+
+
+def process(data):
+
+    for wikipage in data:
+        with open(f'{folder_data}/{wikipage}', 'r', encoding='utf-8') as arq:
+            htmlstring = arq.read()
+
+        #    +====================================+
+        #   || Limpe as tags HTML de htmlstring  ||
+        #   +====================================+
+    
+    return None
+
+results = process(list_data)
+with open('result.txt', 'w', encoding='utf-8') as result:
+    for score in results:
+        result.write(f'{score[0].replace(' - Wikipedia.html', '')} \t : {score[1]}\n')
