28869786001eb08b747b52e76ee0677301efa5e9
CrCardd <cris.rc377@gmail.com>
2025-05-30 11:42:56 -0300

    falta melhorar a aula 16

--- Logica de programacao/aula 16 - estrutura condicional parte 1/Operadores logicos/atv5/atv5.py
+++ Logica de programacao/aula 16 - estrutura condicional parte 1/Operadores logicos/atv5/atv5.py
@@ -8,29 +8,29 @@ idades =    [8,          15,         20,        19,         23          ]
 def play():
     
     if( not (nomes.index("Eduardo") == 2)):
-        return "1"
+        return "Falhou na regra '1'"
     if( not (notas[idades.index(19)] == 3)):
-        return "2"
+        return "Falhou na regra '2'"
     if( not (notas[filmes.index("Encanto")] == 9)):
-        return "3"
+        return "Falhou na regra '3'"
     if( not (filmes[nomes.index("Cristian")] == "Barbie")):
-        return "4"
+        return "Falhou na regra '4'"
     if( not (notas[nomes.index("Eduardo")] == max(notas))):
-        return "5"
+        return "Falhou na regra '5'"
     if( not (idades.index(8) == 0 or idades.index(8) == 4)):
-        return "6"
+        return "Falhou na regra '6'"
     if( not (idades[nomes.index("Donathan")] == max(idades))):
-        return "7"
+        return "Falhou na regra '7'"
     if( not (idades[filmes.index("Encanto")] == min(idades))):
-        return "8"
+        return "Falhou na regra '8'"
     if( not (idades.index(min(idades)) < nomes.index("Nycollas"))):
-        return "9"
+        return "Falhou na regra '9'"
     if( not (filmes[nomes.index("Donathan")] != "Rocky" and filmes[nomes.index("Donathan")] != "Sonic")):
-        return "10"
+        return "Falhou na regra '10'"
     if( not (notas[nomes.index("Cristian") > notas[nomes.index("Nycollas")]] and notas[nomes.index("Cristian") < notas[nomes.index("Leonardo")]])):
-        return "11"
+        return "Falhou na regra '11'"
     if( not (idades[nomes.index("Nycollas")] > idades[nomes.index("Cristian")] and idades[nomes.index("Nycollas")] < idades[filmes.index("Rocky")])):
-        return "12"
+        return "Falhou na regra '12'"
     if( not
        (
         (nomes.index("Nycollas") > nomes.index("Eduardo") and nomes.index("Nycollas") < nomes.index("Donathan")) or 
@@ -40,7 +40,7 @@ def play():
         return "13"
 
     
-    return "Deu boa"
+    return "Resposta correta !!!\n!!!!!!!!!!!!!"
 
 print(play())
 
