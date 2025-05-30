6b60f5369c3f369a0dd6c2a8a25c6a25dfbd14ba
CrCardd <cris.rc377@gmail.com>
2025-05-30 11:21:17 -0300

    aula16/operadoresLogicos/atv5

---
+++ Logica de programacao/aula 16 - estrutura condicional parte 1/Operadores logicos/atv5/atv5.py
@@ -0,0 +1,48 @@
+nomes  =    ["Leonardo", "Cristian", "Eduardo", "Nycollas", "Donathan"  ]
+filmes =    ["Encanto",  "Barbie",   "Rocky",   "Sonic",    "O Irlandês"]
+notas  =    [9,          7,          10,        3,          1           ]
+idades =    [8,          15,         20,        19,         23          ]
+
+
+
+def play():
+    
+    if( not (nomes.index("Eduardo") == 2)):
+        return "1"
+    if( not (notas[idades.index(19)] == 3)):
+        return "2"
+    if( not (notas[filmes.index("Encanto")] == 9)):
+        return "3"
+    if( not (filmes[nomes.index("Cristian")] == "Barbie")):
+        return "4"
+    if( not (notas[nomes.index("Eduardo")] == max(notas))):
+        return "5"
+    if( not (idades.index(8) == 0 or idades.index(8) == 4)):
+        return "6"
+    if( not (idades[nomes.index("Donathan")] == max(idades))):
+        return "7"
+    if( not (idades[filmes.index("Encanto")] == min(idades))):
+        return "8"
+    if( not (idades.index(min(idades)) < nomes.index("Nycollas"))):
+        return "9"
+    if( not (filmes[nomes.index("Donathan")] != "Rocky" and filmes[nomes.index("Donathan")] != "Sonic")):
+        return "10"
+    if( not (notas[nomes.index("Cristian") > notas[nomes.index("Nycollas")]] and notas[nomes.index("Cristian") < notas[nomes.index("Leonardo")]])):
+        return "11"
+    if( not (idades[nomes.index("Nycollas")] > idades[nomes.index("Cristian")] and idades[nomes.index("Nycollas")] < idades[filmes.index("Rocky")])):
+        return "12"
+    if( not
+       (
+        (nomes.index("Nycollas") > nomes.index("Eduardo") and nomes.index("Nycollas") < nomes.index("Donathan")) or 
+        (nomes.index("Nycollas") > nomes.index("Donathan") and nomes.index("Nycollas") < nomes.index("Eduardo"))
+       )
+    ):
+        return "13"
+
+    
+    return "Deu boa"
+
+print(play())
+
+    
+    
